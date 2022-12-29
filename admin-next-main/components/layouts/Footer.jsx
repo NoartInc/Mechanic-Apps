4ed101c4a8920634aa8a3dd9aeed61f5005cc652
Afif <incnoart@gmail.com>
2022-12-29 10:02:17 +0700

    update pages & footer

---
+++ admin-next-main/components/layouts/Footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Footer() {
+  return (
+    <>
+        <hr class="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
+            <span class="block text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2022 <a href="#" class="hover:underline">IT BRM</a>. Star Roof.
+            </span>
+    </>
+  )
+}
+
+export default Footer
